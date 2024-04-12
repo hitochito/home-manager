711127fafe0c359237e80e0545b13e97593e44a7
hitochito <hitochito0@gmail.com>
2024-04-13 03:58:29 +0600

    add git aliases

--- modules/shell.nix
+++ modules/shell.nix
@@ -4,14 +4,30 @@
   home.packages = [
     pkgs.fd #LAZVIM DEPENDENCY
     pkgs.ripgrep #LAZYVIM DEPENDENCY
+    pkgs.devbox #NIX-POWERED DEV ENVIRONMENT
+    pkgs.neofetch #FETCH STATUS OF THE SYSTEM
   ];
 
-  programs.gh.enable = true;
-  programs.git.enable = true;
-  programs.lazygit.enable = true;
-  programs.git.userName = "hitochito";
-  programs.git.userEmail = "hitochito0@gmail.com";
+  programs.git = {
+    enable = true;
+    userName = "hitochito";
+    userEmail = "hitochito0@gmail.com";
+    aliases = {
+      # Ignore Changes Locally From a File.
+      hide = "update-index --assume-unchanged";
+      unhide = "update-index --no-assume-unchanged";
+      unhide-all = "update-index --really-refresh";
 
+      # git add, commit, push Using Single Command
+      acp = "!f() { git add -A && git commit -m \"$@\" && git push; }; f";
+
+      # List Hidden/Ignored Files
+      hidden = "!f(){ git -C \"$GIT_PREFIX\" ls-files -v \"$@\" | grep \"^[a-z]\";}; f";
+      ignored = "!f(){ git -C \"$GIT_PREFIX\" status -s --ignored \"$@\" | grep \"^!!\";}; f";
+    };
+  };
+  programs.gh.enable = true;
+  programs.lazygit.enable = true;
   programs.neovim.enable = true;
   home.sessionVariables = {
     EDITOR = "nvim";
